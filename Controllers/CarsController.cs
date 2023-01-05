308776746b34c449032e5eac569e11bdbb2c6648
toddmac12 <toddmac12@users.noreply.github.com>
2023-01-05 00:50:32 -0500

    Update CarsController.cs

--- Controllers/CarsController.cs
+++ Controllers/CarsController.cs
@@ -4,28 +4,27 @@ using System.Collections.Generic;
 
 namespace CarDealership.Controllers
 {
-  public class CarsController : Controller
-
-  {
-    [HttpGet("/cars")]
-    public ActionResult Index()
+    public class CarsController : Controller
     {
-      List<Car> allCars = Car.GetAll();
-      return View(allCars);
-    }
+        [HttpGet("/cars")]
+        public ActionResult Index()
+        {
+            List<Car> allCars = Car.GetAll();
+            return View(allCars);
+        }
 
-    [HttpGet("/cars/new")]
-    public ActionResult CreateCar()
-    {
-      return View();
-    }
+        [HttpGet("/cars/new")]
+        public ActionResult CreateCar()
+        {
+            return View();
+        }
 
-    [HttpPost("/cars")]
-    public ActionResult Create(string carModel, int price, int miles)
-    {
-      Car myCar = new Car(carModel, price, miles);
-      return RedirectToAction("Index");
+        [HttpPost("/cars")]
+        public ActionResult Create(string carModel, int price, int miles)
+        {
+            Car myCar = new Car(carModel, price, miles);
+            return RedirectToAction("Index");
+        }
     }
-  }
 }
 
